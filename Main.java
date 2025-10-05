484df7f9ae0130d6299fed7c233da6a4bf0363fc
Swaraj Hanumant Akhade <akhadeswaraj1326@gmail.com>
2025-10-05 13:16:40 +0530

    Update Main.java
    Hello

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("this is for jenkins");
+        System.out.println("hello ,this is for jenkins");
         System.out.println("started at: " + new Date());
     }
 }
